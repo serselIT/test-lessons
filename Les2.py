491a402bfde350627fb287af16094e0c4a63a440
serselIT <ser.sel.it@gmail.com>
2022-08-29 19:46:36 +0300

    my first commit

---
+++ Les2.py
@@ -0,0 +1,47 @@
+# #Первый тип коментария
+# # int = ...
+# # float = ...
+# # string = ...
+# # bool = ...
+#
+# number_of_dogs = 10
+# print(type(number_of_dogs))
+# print(type(int))
+#
+#
+# weight = 80.5
+# print(type(weight))
+#
+# language = 'python'
+# print(type(language))
+#
+# work_done = True
+# print(type(work_done))
+#
+# print(number_of_dogs, weight,language,work_done)
+# num = input(Введите число)
+#
+# num_1, num_2, str_1, float_1 = int(input()), int(input()), int(input()), int(input())
+# print(num_1 + num_2 + float_1)
+# print(str_1)
+#
+# #площадь и периметр треугольника
+# a, h = float(input('введите длинну основания')), float(input('введите высоту'))
+# print()
+# print()
+#
+# s5 =
+
+#задача 3
+#  s = 'python'
+#  i = str(10)
+#  f = str(12,3)
+#  b = (false)
+#
+# #зАДАЧА 4
+# #зАДАЧА 5
+
+import random
+print(random.randint(1, 365))
+
+
---
+++ some_file.py
@@ -0,0 +1,18 @@
+import random
+
+
+
+
+print('Hello, World')
+print('Hi')
+print(1+2)
+
+HIGHT = 10
+var = 'python'
+print(var)
+var = 'java'
+print(var)
+
+import flake8
+
+
